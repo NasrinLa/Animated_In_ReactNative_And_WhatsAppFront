63b237cd3f1f37a49ec2b4134d0929347b55eed6
NasrinLa <nasrin.latifi95@gmail.com>
2019-04-15 16:40:50 +0430

    First And Final Commit

---
+++ DataComponent.js
@@ -0,0 +1,19 @@
+export default data=[
+
+
+        {'name': 'Ben', 'id': 1 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Susan', 'id': 2 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Robert', 'id': 3 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Mary', 'id': 4 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Daniel', 'id': 5 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Laura', 'id': 6 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'John', 'id': 7 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Debra', 'id': 8 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Aron', 'id': 9 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Ann', 'id': 10 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Steve', 'id': 11 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')},
+        {'name': 'Olivia', 'id': 12 , 'bio' : "now I'm using whatsapp" , 'imageUrl': require('./accest/Unknown_Person_Male_W.png')}
+   
+
+
+]
---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import {AppRegistry} from 'react-native';
+import Whatapp from './Whatapp';
+import Colorfull from './Colorfull';
+import App2 from './App2';
+import Flatlist from './Flatlist';
+import FlatList2 from './FlatList2';
+import Flattemp from './Flattemp';
+import WhatsAppFlat from './WhatsAppFlat';
+import Temp from './Temp';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Colorfull);
